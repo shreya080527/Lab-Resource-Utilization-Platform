74ecfb2ed035f8f235f22929b353dca5f018b46c
shreya <shreyaama2000@gmail.com>
2026-07-02 00:46:38 +0530

    Initial Spring Boot setup

---
+++ src/test/java/com/example/lab_resource_platform/LabResourcePlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lab_resource_platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabResourcePlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
